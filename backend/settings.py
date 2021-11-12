f71b53c7358d79e166dbd71801f773b7fa1727a3
Aviraj Bevli <bevliaviraj70@gmail.com>
2021-11-13 00:57:39 +0530

    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' was causing probelms

--- backend/settings.py
+++ backend/settings.py
@@ -154,4 +154,4 @@ STATICFILES_DIRS = [
 # added manually
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
